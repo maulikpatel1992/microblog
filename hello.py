9af972e47b7d04f6d771cd6f9c57aca168e28642
Maharaj <mvp46@njit.edu>
2021-03-10 19:02:46 -0500

    Templates, Web forms, Database, User login, Profile

---
+++ hello.py
@@ -0,0 +1,15 @@
+import os
+
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ADMINS = ['ermaulik79@gmail.com']
+
+print(MAIL_SERVER)
+print(MAIL_PORT)
+print(MAIL_USE_TLS)
+print(MAIL_USERNAME)
+print(MAIL_PASSWORD)
+print(ADMINS)
