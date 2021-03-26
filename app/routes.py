1d10cad7b89467b01a6875067e81bba4a2a63760
Maharaj <mvp46@njit.edu>
2021-03-26 05:45:41 -0400

    Added Post field on Home page

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,8 @@ from app import db
 from app.forms import RegistrationForm
 from datetime import datetime
 from app.forms import EditProfileForm, EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
 @app.before_request
 def before_request():
@@ -18,10 +20,17 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
