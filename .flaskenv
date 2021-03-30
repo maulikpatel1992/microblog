baa565bf68415162d78b8fefe762f455220b4a80
Maharaj <mvp46@njit.edu>
2021-03-30 09:07:29 -0400

    Added email support

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,4 @@
 FLASK_APP = microblog.py
+MAIL_SERVER' = smtp.googlemail.com
+MAIL_PORT = 587
+MAIL_USE_TLS = 1
--- config.py
+++ config.py
@@ -6,11 +6,13 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATION = False
     
+     
     #Sending erros by email
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['ermaulik79@gmail.com']
+    ADMINS = ['ms26082ms@gmail.com']
     POST_PER_PAGE = 3
+    
